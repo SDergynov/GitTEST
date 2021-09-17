2ae106ed56e5d31863b2a4de218a67dd4e64c0c5
Дергунов Сергей Дмитриевич <sdergynov@gmail.com>
2021-09-17 11:17:10 +0300

    Dice class has been created. First commit to dev branch.

--- src/by/dergynov/Main.java
+++ src/by/dergynov/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Let's play dice!");
+        Dice firstDice = new Dice();
+        System.out.println(firstDice.throwDice());
     }
 }
