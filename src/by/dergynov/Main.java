1a5650cb5d96c8b8ccba38e2f076848588a9938a
Дергунов Сергей Дмитриевич <sdergynov@gmail.com>
2021-09-17 11:37:58 +0300

    Commit to feature branch

--- src/by/dergynov/Main.java
+++ src/by/dergynov/Main.java
@@ -3,9 +3,16 @@ package by.dergynov;
 public class Main {
 
     public static void main(String[] args) {
+        boolean theSameDice = false;
         System.out.println("Let's play dice!");
         Dice firstDice = new Dice();
         Dice secondDice = new Dice();
-        System.out.printf("Dices is %d:%d",firstDice.throwDice(), secondDice.throwDice());
+        int firstDiceVolume = firstDice.throwDice();
+        int secondDiceVolume = secondDice.throwDice();
+        if(firstDiceVolume==secondDiceVolume){
+            System.out.println("Ups...! It's amassing!");
+        }
+        System.out.printf("Dices is %d:%d",firstDiceVolume, secondDiceVolume);
+
     }
 }
