2add156ffeaf04da1dfd344694e64941cdc75925
Дергунов Сергей Дмитриевич <sdergynov@gmail.com>
2021-09-22 17:51:19 +0300

    Some changes, again.

--- src/by/dergynov/Main.java
+++ src/by/dergynov/Main.java
@@ -7,9 +7,10 @@ public class Main {
         System.out.println("Let's play dice!");
         Dice firstDice = new Dice();
         Dice secondDice = new Dice();
+        int firstDiceVolume,secondDiceVolume;
         while (!theSameDiceVolume)  {
-            int firstDiceVolume = firstDice.throwDice();
-            int secondDiceVolume = secondDice.throwDice();
+            firstDiceVolume = firstDice.throwDice();
+            secondDiceVolume = secondDice.throwDice();
             if (firstDiceVolume == secondDiceVolume) {
                 theSameDiceVolume = true;
                 System.out.println("Ups...! It's amassing!");
